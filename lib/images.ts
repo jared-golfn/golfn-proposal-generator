817af139df8f0f89783e1eb9f77f6610a1773ae1
Jared GolfN <jared@golfn.com>
2026-03-12 10:30:48 -0500

    feat: add new Daily Grind image to images config

--- lib/images.ts
+++ lib/images.ts
@@ -3,6 +3,7 @@ export const images = {
   logo: 'https://cdn.sanity.io/images/e3wja34v/production/3bcfd9b87d10769072b59ff0fe7cbefe7d36286e-3594x860.png',
   logoIcon: 'https://cdn.sanity.io/images/e3wja34v/production/4093e34c1af9c32f46bb63fc2ceed885e1fd8855-1036x998.png',
   offCourse: 'https://cdn.sanity.io/images/e3wja34v/production/9f201313469c4cc0362d686a174246ba51e688c5-750x1624.png',
+  dailyGrind: 'https://cdn.sanity.io/images/e3wja34v/production/b2bc4cf588faf556017e1fb0b83505c1c50ee602-1500x3248.png',
   srixonAd3: 'https://cdn.sanity.io/images/e3wja34v/production/6c836e9b8379876f2c8005584111264f9507c08f-1364x884.png',
   srixonAd1: 'https://cdn.sanity.io/images/e3wja34v/production/b7595e6209bca636cccf5ece0d615cc6331bc151-526x1058.png',
   srixonAd2: 'https://cdn.sanity.io/images/e3wja34v/production/37500f67f5ed6154a60f0859d289dbf49ae53942-526x1058.png',
