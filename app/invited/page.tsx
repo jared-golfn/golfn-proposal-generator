5f1cae5ae05ad3f46bd5e7dc7a27a1c1b35a2ea5
Jared GolfN <jared@golfn.com>
2026-04-21 12:41:47 -0500

    feat: add password gate to Invited page (server-side cookie check)

--- app/invited/page.tsx
+++ app/invited/page.tsx
@@ -1,7 +1,18 @@
-'use client'
-
+import { cookies } from 'next/headers'
 import InvitedPitchPage from '@/components/invited/InvitedPitchPage'
+import { PasswordGate } from '@/components/template/PasswordGate'
 
-export default function InvitedPage() {
+export const dynamic = 'force-dynamic'
+
+const INVITED_PASSWORD = 'golfn2026'
+
+export default async function InvitedPage() {
+  const cookieStore = await cookies()
+  const authCookie = cookieStore.get('golfn-auth-invited')
+
+  if (!authCookie?.value) {
+    return <PasswordGate slug="invited" partnerName="Invited Clubs" />
+  }
+
   return <InvitedPitchPage />
 }
